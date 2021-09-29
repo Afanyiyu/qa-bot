71e8921c7b04dce98860dce38bb9ca64f4d24081
Il Harper <afanyiyu@hotmail.com>
2021-09-29 22:01:01 +0800

    feat: add mysql plugin

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { App } from 'koishi'
 
 import * as pluginOnebot from '@koishijs/plugin-onebot'
 import * as pluginCommon from '@koishijs/plugin-common'
+import * as pluginMysql from '@koishijs/plugin-mysql'
 
 const app = new App()
 
@@ -12,6 +13,15 @@ app.plugin(pluginOnebot, {
   protocol: 'ws',
   selfId: process.env.ONEBOT_SELF_ID,
   endpoint: process.env.ONEBOT_WS_ENDPOINT,
+})
+
+// Configure Mysql
+app.plugin(pluginMysql, {
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: process.env.KOISHI_MYSQL_PASS,
+  database: 'qabot',
 })
 
 // Configure plugin-common
