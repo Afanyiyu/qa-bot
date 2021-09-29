3b00cdd5bf5c6ff6fdd75295249c3cb08cc51464
Il Harper <afanyiyu@hotmail.com>
2021-09-29 20:51:27 +0800

    feat: add onebot plugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import './env'
+
 import { App } from 'koishi'
 
+import * as pluginOnebot from '@koishijs/plugin-onebot'
+
 const app = new App()
+
+// Configure onebot
+app.plugin(pluginOnebot, {
+  protocol: 'ws',
+  selfId: process.env.ONEBOT_SELF_ID,
+  endpoint: process.env.ONEBOT_WS_ENDPOINT,
+})
 
 app.start()
